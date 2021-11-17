a30df12f3416cdc3fc44d191cd72eb5217f4a9a1
Rafael <developerduckarroyo@gmail.com>
2021-11-17 11:27:37 -0800

    safe point from PC

---
+++ js/javaScript.js
@@ -0,0 +1,8 @@
+//Contact form
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}

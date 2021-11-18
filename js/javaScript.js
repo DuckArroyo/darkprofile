dbb52c53f26de500b87a56a8307dd9a795e440e7
Rafael <developerduckarroyo@gmail.com>
2021-11-18 08:55:10 -0800

    sorted code before refactoring

--- js/javaScript.js
+++
@@ -1,8 +0,0 @@
-//Contact form
-const accordion = document.getElementsByClassName('container');
-
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}

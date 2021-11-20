d10c5c220314ee7d8139e61098147c65a1959386
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-19 23:04:11 -0800

    playing with buttons

--- js/app.js
+++ js/app.js
@@ -1,29 +1,5 @@
+
+
 // ======== W3 Schools accordion ========
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-// ======== Free code accordion ========
-const accordion = document.getElementsByClassName('container');
-
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
